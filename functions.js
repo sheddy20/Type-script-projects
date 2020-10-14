62f76d2cde9b7d64cb2ecddbcc89a0b738b4dae4
Abel Shedrack Nicholas <shedrackejeh458@gmail.com>
2020-10-14 11:27:42 +0100

    done

--- functions.js
+++ functions.js
@@ -2,4 +2,23 @@ var myNames = function (first, last, age) {
     return ("my first name is: " + first + ", my last name is: " + last + ", am " + age + " years");
 };
 var validateInfo = myNames('Tristan', 'Thompson', 35);
-console.log(validateInfo);
+// console.log(validateInfo);
+var getDetails = function (name, location) {
+    if (location == undefined) {
+        return name;
+    }
+    return "company name: " + name + ", location: " + location;
+};
+var myAnswer = getDetails('Google');
+// console.log(myAnswer);
+var getItems = function (stuff1, stuff2, price) {
+    if (stuff2 == undefined) {
+        return stuff1;
+    }
+    else if (price == undefined) {
+        return stuff1;
+    }
+    return "buy: " + stuff1 + ", buy: " + stuff2 + ", the prices is: " + price + " dollars";
+};
+var myItems = getItems('Milk');
+console.log(myItems);
--- functions.ts
+++ functions.ts
@@ -4,4 +4,28 @@ let myNames = function (first: string, last: string, age: number):string{
 }
 
 const validateInfo = myNames('Tristan', 'Thompson', 35)
-console.log(validateInfo);
+// console.log(validateInfo);
+
+let getDetails = function (name: string, location?: string): string{
+  if (location == undefined) {
+    return name;
+  }
+  return `company name: ${name}, location: ${location}`
+}
+
+const myAnswer = getDetails('Google',);
+// console.log(myAnswer);
+
+let getItems = function (stuff1: string, stuff2?: string, price?: number): any{
+  if (stuff2 == undefined) {
+    return stuff1;
+
+  } else if (price == undefined) {
+    return stuff1;
+
+  } 
+  return `buy: ${stuff1}, buy: ${stuff2}, the prices is: ${price} dollars`
+}
+
+const myItems = getItems('Milk',)
+console.log(myItems);
