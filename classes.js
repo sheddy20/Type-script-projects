c7eb3e330e3073ecba53cb7fb977663645f6b5c3
Abel Shedrack Nicholas <shedrackejeh458@gmail.com>
2020-10-15 10:37:00 +0100

    done

--- classes.js
+++ classes.js
@@ -1,5 +1,17 @@
-function fetchTodos(mytodos) {
-    console.log(mytodos.title + ", " + mytodos.message);
-}
-var getTodos = { title: 'Take the trash out', message: 'finally' };
-fetchTodos(getTodos);
+// interface Mytodo {
+//   title: string;
+//   message: string;
+// }
+// function fetchTodos(mytodos: any) {
+//   console.log(`${mytodos.title}, ${mytodos.message}`);
+// }
+// let getTodos = { title: 'Take the trash out', message: 'finally' }
+// fetchTodos(getTodos);
+var getFullName = function (firstName, lastName) {
+    if (lastName == undefined) {
+        return firstName;
+    }
+    return ("my firstName is: " + firstName + ", my lastName is: " + lastName);
+};
+var getNames = getFullName('Shedrack');
+console.log(getNames);
--- classes.ts
+++ classes.ts
@@ -1,12 +1,23 @@
-interface Mytodo {
-  title: string;
-  message: string;
+// interface Mytodo {
+//   title: string;
+//   message: string;
+// }
+
+// function fetchTodos(mytodos: any) {
+//   console.log(`${mytodos.title}, ${mytodos.message}`);
+// }
+
+// let getTodos = { title: 'Take the trash out', message: 'finally' }
+
+// fetchTodos(getTodos);
+
+let getFullName = function (firstName: string, lastName?:string): any{
+  
+  if (lastName == undefined) {
+    return firstName; 
+  }
+  return (`my firstName is: ${firstName}, my lastName is: ${lastName}`);
 }
 
-function fetchTodos(mytodos: any) {
-  console.log(`${mytodos.title}, ${mytodos.message}`);
-}
-
-let getTodos = { title: 'Take the trash out', message: 'finally' }
-
-fetchTodos(getTodos);
+const getNames = getFullName('Shedrack',);
+console.log(getNames);
